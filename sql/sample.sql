27b7672692b28c691008686e6abc7b709e576d78
Kirill Zhirnov <ekvixgroup@gmail.com>
2024-01-09 09:03:00 +0100

    bump version

--- sql/sample.sql
+++ sql/sample.sql
@@ -18663,6 +18663,13 @@ CREATE INDEX person_search_lower_idx ON public.person_search USING gin (lower(se
 
 
 --
+-- Name: person_site_id_email_idx; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE UNIQUE INDEX person_site_id_email_idx ON public.person USING btree (site_id, email) WHERE (registered_at IS NOT NULL);
+
+
+--
 -- Name: person_site_id_email_registered_at_idx; Type: INDEX; Schema: public; Owner: -
 --
 
